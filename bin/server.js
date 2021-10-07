84159bd012277434568b41a617acc6708ff0cf86
Andrew-Romanov <andrewromanov@yahoo.com>
2021-10-07 13:03:38 +0300

    Added .env_example

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const app = require('../app')
 
-// const PORT = process.env.PORT || 3000
+// const SERVER_PORT = process.env.PORT || 3000
 const { DATABASE_HOST, SERVER_PORT = 3000 } = process.env
 
 mongoose.connect(DATABASE_HOST,
