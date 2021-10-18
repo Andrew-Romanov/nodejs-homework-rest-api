67ab55da6d9da7bdedae3496adfd43552fbdd906
Andrew-Romanov <andrewromanov@yahoo.com>
2021-10-18 15:45:12 +0300

    Pagination added

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,8 +3,15 @@ const { InternalServerError } = require('http-errors')
 const { Contact } = require('../../models')
 
 const getAll = async (req, res, next) => {
+  let { page = 1, limit = 20 } = req.query
+
+  page = parseInt(page)
+  limit = parseInt(limit)
+
   const contactsData = await Contact
-    .find({ owner: req.user._id }, '_id name email phone favorite owner')
+    .find({ owner: req.user._id },
+      '_id name email phone favorite owner',
+      { skip: (page - 1) * limit, limit: limit })
     .populate('owner', 'email subscription')
 
   if (!contactsData) throw new InternalServerError('Server error')
